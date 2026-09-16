359783adbd1fe5f4afe828c084fdfd05a755365c
ePianos <epianos@ePianoss-MacBook-Pro.local>
2026-09-16 09:22:56 +0100

    Removed errant spacer blocks

--- patterns/frontpage-hero.php
+++ patterns/frontpage-hero.php
@@ -6,28 +6,31 @@
  */
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
+<!-- wp:group {"className":"front-page-hero","layout":{"type":"constrained"}} -->
 <div class="wp-block-group front-page-hero">
 
     <!-- wp:spacer {"height":"25px"} -->
     <div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
     <!-- /wp:spacer -->
 
-    <!-- wp:columns {"verticalAlignment":"center","align":"wide","className":"front-hero","style":{"spacing":{"padding":{"right":"var:preset|spacing|hero-padding","left":"var:preset|spacing|hero-padding"}}}} -->
-    <div class="wp-block-columns alignwide are-vertically-aligned-center front-hero"
-        style="padding-right:var(--wp--preset--spacing--hero-padding);padding-left:var(--wp--preset--spacing--hero-padding)">
+    <!-- wp:columns {"verticalAlignment":"center","align":"wide","className":"front-hero"} -->
+    <div class="wp-block-columns alignwide are-vertically-aligned-center front-hero">
 
         <!-- wp:column {"verticalAlignment":"center"} -->
         <div class="wp-block-column is-vertically-aligned-center">
 
-            <!-- wp:group {"tagName":"main","className":"front-hero-content","layout":{"type":"default"}} -->
-            <main class="wp-block-group front-hero-content">
+            <!-- wp:group {"className":"front-hero-content","layout":{"type":"default"}} -->
+            <div class="wp-block-group front-hero-content">
 
-                <!-- wp:site-title {"level":2,"className":"hero-text","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"spacing":{"padding":{"right":"0","left":"0"}}},"textColor":"contrast","fontSize":"hero"} /-->
+                <!-- wp:site-title {"level":2,"className":"hero-text","textColor":"contrast","fontSize":"hero"} /-->
 
-                <!-- wp:site-tagline {"className":"hero-tagline","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"textColor":"contrast"} /-->
+                <!-- wp:paragraph {"className":"hero-tagline","backgroundColor":"neon","textColor":"contrast"} -->
+                <p class="hero-tagline has-contrast-color has-neon-background-color has-text-color has-background">A
+                    minimalist, brutalist, lightning-fast theme with a custom live RESTful search block, a custom
+                    associated page/post block, and a custom promo banner.</p>
+                <!-- /wp:paragraph -->
 
-            </main>
+            </div>
             <!-- /wp:group -->
 
         </div>
@@ -36,10 +39,9 @@
         <!-- wp:column {"verticalAlignment":"center"} -->
         <div class="wp-block-column is-vertically-aligned-center">
 
-            <!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","align":"center","className":"hero-image"} -->
+            <!-- wp:image {"sizeSlug":"full","linkDestination":"none","align":"center","className":"hero-image"} -->
             <figure class="wp-block-image aligncenter size-full hero-image">
-                <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/TMaeM.jpg' ) ); ?>" alt=""
-                    style="aspect-ratio:1;object-fit:cover" />
+                <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/TMaeM.jpg' ) ); ?>" alt="" />
             </figure>
             <!-- /wp:image -->
 
